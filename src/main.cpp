af10ba0252d4178cea0211f5193dff050307a497
Nikita Chigvintsev <nikmail007@gmail.com>
2020-07-20 15:10:24 +0700

    Initialized Sfml body

---
+++ src/main.cpp
@@ -0,0 +1,36 @@
+#include <SFML/Graphics.hpp>
+#include <iostream>
+#include <math.h>
+#include <sstream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+
+using namespace std;
+using namespace sf;
+
+int main()
+{
+    RenderWindow window(VideoMode(1920, 1080), "Snake 2020", Style::Fullscreen);
+    view.reset(FloatRect(0, 0, 1920, 1080));
+
+    Image mapImage;
+    mapImage.loadFromFile("images/Tiles.png");
+    Texture mapTexture;
+    mapTexture.loadFromImage(mapImage);
+    Sprite map;
+    map.setTexture(mapTexture);
+
+    Image heroImage;
+    heroImage.loadFromFile("images/Tiles.png");
+
+    Clock clock;
+    while (window.isOpen()) {
+        Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == Event::Closed) {
+                window.close();
+            }
+        }
+        return 0;
+    }
